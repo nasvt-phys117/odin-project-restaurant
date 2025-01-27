2541d9c080d4ba479c6eab780dac9466ad7b154d
nasvt-phys117 <63495021+nasvt-phys117@users.noreply.github.com>
2025-01-27 12:39:22 +0200

    Added Content Elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import "./styles.css";
 console.log("Js seems to be working!");
+
+// import restaurantImage from  "./images/restaurant.jpg";
+
+// const image = document.createElement("img");
+// image.src = restaurantImage;
+
+// document.body.appendChild(image);
